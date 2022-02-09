5ae30ebac6dfc71e986e078492cee3a6dcd2c465
john <johnarokyaraj@yahoo.in>
2022-02-10 00:10:57 +0530

    header changes

--- biddingmanagerapp/src/app/app.module.ts
+++ biddingmanagerapp/src/app/app.module.ts
@@ -4,17 +4,37 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DatePipe } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+
+
+
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      HeaderComponent,
+      HomeComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes,
+      { enableTracing: false }
+    ),
+    MatToolbarModule,
+
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
