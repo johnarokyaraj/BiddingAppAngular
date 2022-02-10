cb9ca8021bb6067dfb0ceb5ce072ed850410bd51
john <johnarokyaraj@yahoo.in>
2022-02-11 00:00:15 +0530

    Initial Screen Design

--- biddingmanagerapp/src/app/app.module.ts
+++ biddingmanagerapp/src/app/app.module.ts
@@ -4,18 +4,38 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+
+
 import { MatToolbarModule } from '@angular/material/toolbar';
-
-
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { CdkTableModule } from '@angular/cdk/table';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatPaginatorModule} from '@angular/material/paginator';
+// import { NgxMatDatetimePickerModule, NgxMatTimepickerModule,NgxMatNativeDateModule } from '@angular-material-components/datetime-picker';
+// import { MatMomentDateModule } from '@angular/material-moment-adapter';
 
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
+  { path: "",   redirectTo: '/home', pathMatch: 'full' }, // redirect to `first-component`
+  { path:"home", component:HomeComponent},
 ];
 @NgModule({
   declarations: [		
@@ -32,9 +52,35 @@ const appRoutes: Routes = [
       { enableTracing: false }
     ),
     MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatTableModule,
+    CdkTableModule,
+    HttpClientModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+     MatPaginatorModule//,
+    // MatMomentDateModule,
+    // NgxMatDatetimePickerModule,
+    // NgxMatTimepickerModule,
+    // NgxMatNativeDateModule
 
   ],
-  providers: [DatePipe],
+  providers: [DatePipe,
+    MatTableModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatDialog],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- biddingmanagerapp/src/app/home/home.component.ts
+++ biddingmanagerapp/src/app/home/home.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,
+    private formBuilder: FormBuilder,
+    public datepipe: DatePipe) { }
 
   ngOnInit() {
   }
